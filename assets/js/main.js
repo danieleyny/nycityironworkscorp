e85014bd2f2d32b3dbcf837d631a4e332b4fe0cb
Daniel eyny <danieleyny@daniels-mbp.home>
2021-07-30 03:32:03 -0400

    additional layers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,68 @@
-/*===== MENU SHOW Y HIDDEN =====*/
-const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('nav-toggle'),
-    closeMenu = document.getElementById('nav-close')
+var navLinks = document.getElementById("navLinks");
 
-// SHOW
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
-
-// HIDDEN
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
-
-/*===== MOUSEMOVE HOME IMG =====*/
-document.addEventListener('mousemove', move);
-function move(e){
-    this.querySelectorAll('.move').forEach(layer =>{
-        const speed = layer.getAttribute('data-speed')
-
-        const x = (window.innerWidth - e.pageX*speed)/120
-        const y = (window.innerHeight - e.pageY*speed)/120
-
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
+function showMenu(){
+    navLinks.style.right = "0";
 }
 
-/*===== GSAP ANIMATION =====*/
-// NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
 
-// HOME
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+
+
+
+
+
+
+
+$('#ask-a-question').on('click', () => {
+    const contactDivPosition = document.getElementById('contact').offsetTop;
+    window.scroll({
+      top: contactDivPosition + 30,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  $('#property-image-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    infinite: true,
+    prevArrow: '<a href="#" class="slick-arrow slick-prev"><span class="fa fa-chevron-left"></span></a>',
+    nextArrow: '<a href="#" class="slick-arrow slick-next"><span class="fa fa-chevron-right"></span></a>'
+  });
+
+  $('#properties-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<a href="#" class="slick-arrow slick-prev">previous</a>',
+    nextArrow: '<a href="#" class="slick-arrow slick-next">next</a>',
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 530,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+  });
