2ed7f25eb64cf3f70030c98a0e05341907db4589
Daniel eyny <danieleyny@daniels-mbp.home>
2021-08-06 05:21:28 -0400

    gallery page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,29 @@ $('#ask-a-question').on('click', () => {
       }
     ]
   });
+
+
+  // gallery item filter
+  
+  const filterButtons = document.querySelector("#filter-btns").children;
+  const items = document.querySelector(".portfolio-gallery").children;
+
+  for(let i =0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function(){
+      for (let j =0; j < filterButtons.length; j++) {
+        filterButtons[j].classList.remove("active2")
+      }
+      this.classList.add("active2");
+      const target = this.getAttribute("data-target")
+
+      for (let k = 0; k < items.length; k++) {
+        items[k].style.display = "none";
+        if (target == items[k].getAttribute("data-id")) {
+          items[k].style.display = "block";
+        }
+        if (target == "all") {
+          items[k].style.display = "block";
+        }
+      }
+    })
+  }
